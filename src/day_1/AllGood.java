8e2c6b652c20b0fb4c8db8a28cd6a11bba1395cd
fulukaya <famolux@Gmail.com>
2020-04-28 15:34:57 -0400

    add AllGood.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -3,6 +3,10 @@ package day_1;
 public class AllGood {
     public static void main(String[] args) {
         //I hope everything is GOOD.
+
+        //changes
+
+
     }
 
 
--- src/day_1/AllGood2.java
+++ src/day_1/AllGood2.java
@@ -3,5 +3,11 @@ package day_1;
 public class AllGood2 {
     public static void main(String[] args) {
         System.out.println("hello");
+
+
+
+
+
+
     }
 }
